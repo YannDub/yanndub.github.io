c9cb445e8265d94d825cb08877c6f1851a16a5c1
YannDub <yann59.dubois@gmail.com>
2019-05-08 16:12:46 +0200

    Updates

---
+++ precache-manifest.4332786ee8da12a625965bc904c3f063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6bd881f7c4f06e80b60aaa7240663e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a489ca242ba34181a06",
+    "url": "/static/css/main.9cd3b839.chunk.css"
+  },
+  {
+    "revision": "2e794e721a9230320035",
+    "url": "/static/js/2.ad4bf563.chunk.js"
+  },
+  {
+    "revision": "4a489ca242ba34181a06",
+    "url": "/static/js/main.6fc8bbb9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380adf610922866d450d1a7155a60b54.js"
+  "/precache-manifest.4332786ee8da12a625965bc904c3f063.js"
 );
 
 self.addEventListener('message', (event) => {
